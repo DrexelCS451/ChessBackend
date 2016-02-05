4520d301e7eaae7bb75a99d17b694d61f803f6f7
Matthew D'Amore <msd88@drexel.edu>
2016-02-05 17:29:23 -0500

    seeing if the server will run
    WHEEE

--- simple-service/src/main/java/com/example/Main.java
+++ simple-service/src/main/java/com/example/Main.java
@@ -35,11 +35,11 @@ public class Main {
      * @throws IOException
      */
     public static void main(String[] args) throws IOException {
-        /*final HttpServer server = startServer();
+        final HttpServer server = startServer();
         System.out.println(String.format("Jersey app started with WADL available at "
                 + "%sapplication.wadl\nHit enter to stop it...", BASE_URI));
         System.in.read();
-        server.stop();*/
+        server.stop();
         DbUtil d = new DbUtil();
         d.create();
     }
